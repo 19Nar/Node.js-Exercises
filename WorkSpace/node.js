a955fb59b8820dc172848ce8268b7af1749e6671
Narine Vanbeginne <60644251+19Nar@users.noreply.github.com>
2020-05-25 12:20:56 +0200

    Add files via upload

---
+++ WorkSpace/node.js
@@ -0,0 +1,46 @@
+// Working in the console
+
+/*
+Make a timeTillChristmas program that tells you how many seconds there are until Christmas.
+
+today=new Date();
+var cmas=new Date(today.getFullYear(), 11, 25);
+if (today.getMonth()==11 && today.getDate()>25)
+{
+cmas.setFullYear(cmas.getFullYear()+1);
+}
+var one_day=1000*60*60*24;
+console.log(Math.ceil((cmas.getTime()-today.getTime())/(one_day))+
+" days left until Christmas!");
+*/
+
+/*
+Create a node program that tells you the current time and date.
+
+new Date(2022,11,25);
+*/
+
+/*
+Find the current time.
+
+new Date();
+*/
+
+/*
+Add 10 + 10.
+Use console.log('hello world') to output hello world.
+Find out how many seconds there are in a year. 
+How many seconds there are in a century. 
+*/
+
+/*REPL stands for Read Eval Print Loop and it represents a computer environment like a Windows console or Unix/Linux shell where a command is entered and the system responds with an output in an interactive mode. Node.js or Node comes bundled with a REPL environment. It performs the following tasks −
+
+Read − Reads user's input, parses the input into JavaScript data-structure, and stores in memory.
+
+Eval − Takes and evaluates the data structure.
+
+Print − Prints the result.
+
+Loop − Loops the above command until the user presses ctrl-c twice.
+
+The REPL feature of Node is very useful in experimenting with Node.js codes and to debug JavaScript codes.*/
